45e36ce6e92642e3ae01be976aac52399ab5161b
Redwan Ahmed <146144832+redwan2003-bot@users.noreply.github.com>
2026-03-18 02:31:14 +0600

    fix: resolve GSAP import module errors for Vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,22 @@ import { CustomEase } from "gsap/CustomEase";
 import { CustomBounce } from "gsap/CustomBounce";
 import { CustomWiggle } from "gsap/CustomWiggle";
 import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
-import { Draggable } from "gsap/draggable";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Draggable from "gsap/Draggable";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { EaselPlugin } from "gsap/EaselPlugin";
-import { Flip } from "gsap/flip";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Flip from "gsap/Flip";
 import { GSDevTools } from "gsap/GSDevTools";
 import { InertiaPlugin } from "gsap/InertiaPlugin";
 import { MotionPathHelper } from "gsap/MotionPathHelper";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-import { Observer } from "gsap/observer";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Observer from "gsap/Observer";
 import { Physics2DPlugin } from "gsap/Physics2DPlugin";
 import { PhysicsPropsPlugin } from "gsap/PhysicsPropsPlugin";
 import { PixiPlugin } from "gsap/PixiPlugin";
