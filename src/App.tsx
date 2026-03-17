62b49f6cd510877ed964e09fddcbdc59b09c4ee2
Redwan Ahmed <146144832+redwan2003-bot@users.noreply.github.com>
2026-03-18 02:15:27 +0600

    feat: install gsap and add plugin registration in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,63 @@
 import { lazy, Suspense } from "react";
 import "./App.css";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { CustomEase } from "gsap/CustomEase";
+import { CustomBounce } from "gsap/CustomBounce";
+import { CustomWiggle } from "gsap/CustomWiggle";
+import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
+import { Draggable } from "gsap/draggable";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { Flip } from "gsap/flip";
+import { GSDevTools } from "gsap/GSDevTools";
+import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { Observer } from "gsap/observer";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+import { PhysicsPropsPlugin } from "gsap/PhysicsPropsPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { SplitText } from "gsap/SplitText";
+import { TextPlugin } from "gsap/TextPlugin";
 
 const CharacterModel = lazy(() => import("./components/Character"));
 const MainContainer = lazy(() => import("./components/MainContainer"));
 import { LoadingProvider } from "./context/LoadingProvider";
+
+gsap.registerPlugin(
+  useGSAP,
+  Draggable,
+  DrawSVGPlugin,
+  EaselPlugin,
+  Flip,
+  GSDevTools,
+  InertiaPlugin,
+  MotionPathHelper,
+  MotionPathPlugin,
+  MorphSVGPlugin,
+  Observer,
+  Physics2DPlugin,
+  PhysicsPropsPlugin,
+  PixiPlugin,
+  ScrambleTextPlugin,
+  ScrollTrigger,
+  ScrollSmoother,
+  ScrollToPlugin,
+  SplitText,
+  TextPlugin,
+  RoughEase,
+  ExpoScaleEase,
+  SlowMo,
+  CustomEase,
+  CustomBounce,
+  CustomWiggle
+);
 
 const App = () => {
   return (
